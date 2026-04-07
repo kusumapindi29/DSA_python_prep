4ac7e4f04764ba377d4c7d03e165583c78a0c174
kusumapindi29 <kusuma.pindi29@gmail.com>
2026-04-07 22:55:33 +0530

    Update SubarraySumEqualsK.py

--- SubArrayProblems/PrefixSum/SubarraySumEqualsK.py
+++ SubArrayProblems/PrefixSum/SubarraySumEqualsK.py
@@ -20,3 +20,19 @@ We store prefix sums in a hashmap along with their frequencies to count
 all such subarrays efficiently.
 '''
 
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        h={}    # to store prefix sum
+        h[0]=1  # basically we add the prefix of the array as zero
+        s=0     # sum of the number
+        r=0     # number of subarray whose sum equals k
+        for i in range(0,len(nums)):
+            s+=nums[i]
+            if (s-k) in h:
+                r+=h[s-k]
+            if s in h:
+                h[s]+=1
+            else:
+                h[s]=1
+        return r
+
